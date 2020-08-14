33ec81278799071b5b02de0485c6613d6bf4f44f
ratchakoonprn <ratchakoon.prn@outlook.com>
2020-08-14 15:33:26 +0700

    Asynchronous Operation

---
+++ 02/promise-all.js
@@ -0,0 +1,20 @@
+const myPromise1 = new Promise((resolove,reject)=>{
+    setTimeout(()=>{
+        console.log('First');
+        resolove(1);
+    },3000);
+});
+const myPromise2 = new Promise((resolove,reject)=>{
+    setTimeout(()=>{
+        console.log('Second');
+        //resolove(2);
+        reject(new Error('Error!!!'));
+    },2000);
+});
+Promise.all([myPromise1,myPromise2])
+.then((results)=>{
+    console.log(results);
+})
+.catch((error)=>{
+    console.log(error.message);
+});
